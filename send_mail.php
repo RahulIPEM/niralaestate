945341576b417770c90ae0b4d62752f67d5c0a10
Rahul <rahul@Rahuls-MacBook-Air.local>
2020-10-28 07:01:01 +0530

    done: mail sent

--- send_mail.php
+++ send_mail.php
@@ -1,39 +1,44 @@
 <?php
-$msg="";
+try {
+    //code...
+    $return = $_POST;
 
-	$name=($_POST['name']);
-	$email=($_POST['email']);
-	$mobile=($_POST['phone']);
+    $name=($_POST['name']);
+    $email=($_POST['email']);
+    $mobile=($_POST['phone']);
     $comment=($_POST['content']);
-	
-	$html="<table><tr><td>Name</td><td>".$name."</td></tr><tr><td>Email</td><td>".$email."</td></tr><tr><td>Mobile</td><td>".$mobile."</td></tr><tr><td>Comment</td><td>".$comment."</td></tr></table>";
-	
-	include('./PHPMailer-master/PHPMailerAutoload.php');
-	$mail=new PHPMailer(true);
-	$mail->isSMTP();
-	$mail->Host="smtp.gmail.com";
-	$mail->Port=587;
-	$mail->SMTPSecure="tls";
-	$mail->SMTPAuth=true;
-	$mail->Username="kundans48@gmail.com";
-	$mail->Password="ramadhar@123";
-	$mail->SetFrom($name);
-	$mail->addAddress("kundans48@gmail.com");
-	$mail->IsHTML(true);
-	$mail->Subject="New Contact Us";
-	$mail->Body=$html;
-	$mail->SMTPOptions=array('ssl'=>array(
-		'verify_peer'=>false,
-		'verify_peer_name'=>false,
-		'allow_self_signed'=>false
-    ));
-	if($mail->send()){
-        echo json_encode(array('type'=>'message', 'text' => 'Hi '.$name .', thank you for the Showing Interest. We will get back to you shortly.'));
-	    // echo $output;
-		//echo "Mail send";
-	}else{
-        echo json_encode(array('type'=>'error', 'text' => 'Unable to send email, please contact'));
-	    // echo $output;
-		//echo "Error occur";
-	}
+
+    require './PHPMailer-master/PHPMailerAutoload.php';
+
+    $html="<table><tr><td>Name</td><td>".$name."</td></tr><tr><td>Email</td><td>".$email."</td></tr><tr><td>Mobile</td><td>".$mobile."</td></tr><tr><td>Comment</td><td>".$comment."</td></tr></table>";
+    $mail = new PHPMailer(true);
+    $mail->isSMTP(true);
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 587;
+    $mail->SMTPSecure = 'tls';
+    $mail->SMTPAuth = true;
+    $mail->Username="kundans48@gmail.com";
+    $mail->Password="*********";
+    $mail->SetFrom($email, $name);
+    // change add address email to the receipient you want to sent email
+    $mail->addAddress('rs5802@gmail.com', 'Rahul Sharma'); // Add a recipient
+    $mail->isHTML(true);
+    $mail->Subject = 'Query from '. $name;
+    $mail->Body = $html;
+
+    $mail->AltBody = 'Name : ' . $name . ', Email : ' . $email . ', Mobile : ' . $mobile . ', Message:' . $comment;
+
+    if (!$mail->send())
+    {
+        $output = json_encode(array('type'=>'error', 'text' => 'Unable to send email, please contact to support.'));
+        die($output);
+    }
+    else
+    {
+        $output = json_encode(array('type'=>'message', 'text' => 'Hi '.$name .', thank you for the Showing Interest. We will get back to you shortly.'));
+        die($output);
+    }
+} catch (\Throwable $th) {
+    echo json_encode(array('type'=>'error', 'text' => 'Unable to send email, please contact'. $th));
+}
 ?>
